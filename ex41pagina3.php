9f477434c311ca2672bf5ecfaaad09e62b130611
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-05 19:57:57 +0200

    Hitorial y intentos

--- ex41pagina3.php
+++ ex41pagina3.php
@@ -12,25 +12,59 @@
 
     <?php
         session_start();
-        function formulari(){
+
+        if (!isset($_SESSION["historial"])) {
+            $_SESSION["historial"] = array(); // Inicializa el historial como un arreglo vacío si no existe
+        }
+
+        $numeroIntentos = 0;
+        if (!isset($_SESSION["intentos"])) {
+            $_SESSION["intentos"] = $numeroIntentos; // Inicializa el número de intentos si no existe
+        }
+
+        function formulari()
+        {
+
             echo "<form method='POST'>";
             echo "<input type='number' name='endevina'>";
             echo "<input type='submit' value='ENDEVINA'>";
             echo "</form>";
+
+            if (isset($_POST['endevina'])) {
+                // Intentos:
+                $numeroIntentos = $_SESSION["intentos"];
+                ++$numeroIntentos;
+                $_SESSION["intentos"] = $numeroIntentos;
+                echo "Número intentos: $numeroIntentos<br>";
+
+                // Agregar el número ingresado al historial
+                
+                $_SESSION["historial"][]  = $_POST['endevina'];;
+            }
+
+            echo "<h3>Historial:</h3>";
+            echo "<p>";
+            foreach ($_SESSION["historial"] as $numero) {
+                // Imprime los números del historial separados por coma
+                echo "$numero, ";
+            }
+            echo "</p>";
         }
-        if (!isset($_POST["endevina"])){
+
+        if (!isset($_POST["endevina"])) {
             formulari();
         } else {
-            if ($_POST['endevina']===$_SESSION['ocult']){
+            if ($_POST['endevina'] === $_SESSION['ocult']) {
                 echo "<h3>Felicitats</h3>";
                 echo "<a href='ex41pagina1.php'>Tornar a l'inici</a>";
-            } elseif ($_POST['endevina']>$_SESSION['oculta']) {
-                echo "<h3>El lumero que estas buscant es menor</h3>";
+                session_destroy();
+            } elseif ($_POST['endevina'] > $_SESSION['oculta']) {
+                echo "<h3>El número que estás buscando es menor</h3>";
                 formulari();
             }
         }
+?>
 
-    ?>
 
 
 
